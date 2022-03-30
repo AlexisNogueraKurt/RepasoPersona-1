41611bf28f6813078c4fdfd1ed1c6cfecc561675
Alexis Noguera <87494818+AlexisNogueraKurt@users.noreply.github.com>
2022-03-30 15:50:23 -0300

    Acomode los dos punto

--- RepasoPersona.Core/Cuenta.cs
+++ RepasoPersona.Core/Cuenta.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace RepasoPersona.Core
 {
-    public class Cuenta: CuentaCliente
+    public class Cuenta : CuentaCliente
     {
         public int CBU {get; set;}
         public Persona Cliente {get; set;}
