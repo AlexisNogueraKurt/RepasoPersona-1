1ca2543322695345e26c69b1ad6f4b0822755f1f
Alexis Noguera <87494818+AlexisNogueraKurt@users.noreply.github.com>
2022-03-30 15:40:16 -0300

    Abstracción y Cuenta

---
+++ RepasoPersona.Core/Cuenta.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace RepasoPersona.Core
+{
+    public class Cuenta: CuentaCliente
+    {
+        public int CBU {get; set;}
+        public Persona Cliente {get; set;}
+    }
+
+}
+    
+
+
+
+        
